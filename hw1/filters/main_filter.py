08ea5a6850f502b8a89079a84d28e4b9796e7572
Ilija Bunche <ibunceski2003@gmail.com>
2024-11-07 15:27:26 +0100

    Adding missing functionality - appending data

--- hw1/filters/main_filter.py
+++ hw1/filters/main_filter.py
@@ -26,30 +26,41 @@ class StockDataManager:
     def process_issuer(self, issuer):
         storage_data = self.storage.load_data()
         from_date = storage_data.get(issuer)
+        output_file = self.output_dir / f"{issuer}.csv"
+
+        if from_date and output_file.exists():
+            existing_df = pd.read_csv(output_file)
+            existing_df['datetime_parsed'] = pd.to_datetime(existing_df['datetime_parsed'])
+        else:
+            existing_df = pd.DataFrame()
 
         data = self.scraper.scrape_issuer_data(issuer, from_date)
-
         if data:
-            df = pd.DataFrame(data)
-            df['datetime_parsed'] = pd.to_datetime(df['Date'], format="%m/%d/%Y")
-            df['Date'] = df['datetime_parsed'].dt.strftime('%d.%m.%Y')
+            new_df = pd.DataFrame(data)
+            new_df['datetime_parsed'] = pd.to_datetime(new_df['Date'], format="%m/%d/%Y")
+            new_df['Date'] = new_df['datetime_parsed'].dt.strftime('%d.%m.%Y')
 
-            # change
-            price_columns = ['Last trade price', 'Max', 'Min', "%chg.", 'Avg. Price', 'Turnover in BEST in denars',
-                             'Total turnover in denars']
+            price_columns = ['Last trade price', 'Max', 'Min', "%chg.", 'Avg. Price',
+                             'Turnover in BEST in denars', 'Total turnover in denars']
             for col in price_columns:
-                df[col] = df[col].apply(self.format_price)
+                new_df[col] = new_df[col].apply(self.format_price)
 
-            df = df.sort_values(by="datetime_parsed")
+            try:
+                if not existing_df.empty:
+                    combined_df = pd.concat([existing_df, new_df]).drop_duplicates(subset=['Date']).sort_values(
+                        by='datetime_parsed')
+                else:
+                    combined_df = new_df.sort_values(by='datetime_parsed')
 
-            output_file = self.output_dir / f"{issuer}.csv"
-            df.to_csv(output_file, index=False)
+                combined_df.to_csv(output_file, index=False)
 
-            self.storage.update_issuer(issuer, date.today().strftime("%m/%d/%Y"))
+                self.storage.update_issuer(issuer, date.today().strftime("%m/%d/%Y"))
 
-            print(f"Successfully saved {len(df)} records for {issuer}")
+                print(f"Successfully saved {len(new_df)} new records for {issuer}")
+            except Exception as e:
+                print(f'Error in saving the data {e}')
         else:
-            print(f"No data collected for {issuer}")
+            print(f"No new data collected for {issuer}")
 
     def process_all_issuers(self, max_workers=8):
         issuers = IssuerFilter.get_all_issuers()
